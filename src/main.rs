0226c31c2da64aef10b9c305f7d9c098cfb51967
Ramon Brand <ramon2009@gmail.com>
2022-07-07 22:01:49 +0300

    Add string serialization

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use std::{mem::size_of, slice::from_raw_parts_mut};
+use std::mem::size_of;
 
 use serde::{Deserialize, Serialize};
 use serde_json::{Map, Value};
 
-#[repr(C, packed)]
-#[derive(Serialize, Deserialize, Debug, Copy, Clone)]
+#[repr(C)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Person {
     age: u8,
     height: u16,
+    name: String,
 }
 
 fn get_in_slice<'a, T>(slice: &'a [u8], offset: usize) -> Option<&'a T> {
@@ -20,10 +21,6 @@ fn get_in_slice<'a, T>(slice: &'a [u8], offset: usize) -> Option<&'a T> {
     Some(value)
 }
 
-unsafe fn from_bytes<T>(bytes: &mut Vec<u8>) -> &mut [T] {
-    from_raw_parts_mut(bytes.as_mut_ptr() as *mut T, bytes.len() / size_of::<T>())
-}
-
 struct Layout {
     name: String,
     kind: LayoutKind,
@@ -33,6 +30,7 @@ struct Layout {
 
 enum LayoutKind {
     Struct,
+    String,
     Bool,
     U8,
     U16,
@@ -56,6 +54,7 @@ impl Layout {
                 }
                 total_size
             }
+            LayoutKind::String => 0,
             LayoutKind::Bool => 1,
             LayoutKind::U8 => 1,
             LayoutKind::U16 => 2,
@@ -79,6 +78,14 @@ fn build_value_from_layout(layout: &Layout, bytes: &[u8]) -> Value {
                 value.insert(field.name.clone(), build_value_from_layout(&field, &bytes));
             }
             Value::from(value)
+        }
+        LayoutKind::String => {
+            let start = layout.offset as usize;
+            let length_bytes: [u8; 8] = bytes[start..(start + 8)].try_into().unwrap();
+            let length = usize::from_le_bytes(length_bytes);
+            let start = start + 8;
+            let end = start + length;
+            Value::from(String::from_utf8(bytes[start..end].to_vec()).unwrap())
         }
         LayoutKind::Bool => {
             Value::from(*get_in_slice::<u8>(&bytes[(layout.offset as usize)..], 0).unwrap())
@@ -122,6 +129,19 @@ fn build_bytes_from_layout(layout: &Layout, value: &Value) -> Vec<u8> {
         let field_value = value.as_object().unwrap().get(&field.name).unwrap();
         let field_bytes = match field.kind {
             LayoutKind::Struct => build_bytes_from_layout(&field, field_value),
+            LayoutKind::String => {
+                let field_value_string = field_value.as_str().unwrap();
+                let mut bytes = Vec::new();
+                let length_bytes = field_value_string.len().to_le_bytes();
+                for byte in length_bytes.iter() {
+                    bytes.push(*byte);
+                }
+                let string_bytes = field_value_string.as_bytes();
+                for byte in string_bytes.iter() {
+                    bytes.push(*byte);
+                }
+                bytes
+            }
             LayoutKind::Bool => {
                 if field_value.as_bool().unwrap() {
                     vec![1u8]
@@ -164,24 +184,25 @@ fn build_bytes_from_layout(layout: &Layout, value: &Value) -> Vec<u8> {
 }
 
 fn serialize(layout: &Layout, bytes: &Vec<u8>) -> String {
-    let object_size = layout.size_in_bytes() as usize;
-    let value = if bytes.len() > object_size {
-        let object_count = bytes.len() / object_size;
-        let mut value_array = Vec::new();
-        for i in 0..object_count {
-            let value =
-                build_value_from_layout(layout, &bytes[(i * layout.size_in_bytes() as usize)..]);
-            value_array.push(value);
-        }
-        Value::from(value_array)
-    } else {
-        build_value_from_layout(layout, &bytes)
-    };
+    //let object_size = layout.size_in_bytes() as usize;
+    //let value = if bytes.len() > object_size {
+    //let object_count = bytes.len() / object_size;
+    //let mut value_array = Vec::new();
+    //for i in 0..object_count {
+    //let value =
+    //build_value_from_layout(layout, &bytes[(i * layout.size_in_bytes() as usize)..]);
+    //value_array.push(value);
+    //}
+    //Value::from(value_array)
+    //} else {
+    //build_value_from_layout(layout, &bytes)
+    //};
+    let value = build_value_from_layout(layout, &bytes);
     serde_json::to_string(&value).unwrap()
 }
 
-fn deserialize(layout: &Layout, serial: String) -> Vec<u8> {
-    let value = serde_json::from_str::<Value>(&serial).unwrap();
+fn deserialize(layout: &Layout, serial: &String) -> Vec<u8> {
+    let value = serde_json::from_str::<Value>(serial).unwrap();
     if value.is_array()
     /* && layout.kind != LayoutKind::Array */
     {
@@ -199,10 +220,11 @@ fn deserialize(layout: &Layout, serial: String) -> Vec<u8> {
 }
 
 fn main() {
-    let person = Person {
-        age: 27,
-        height: 180,
-    };
+    //
+    // Typed --Bincode--> Bytes --Custom--> String
+    // String --Custom--> Bytes --Bincode--> Typed
+    //
+
     let person_layout = Layout {
         name: String::from("Person"),
         kind: LayoutKind::Struct,
@@ -220,48 +242,79 @@ fn main() {
                 offset: 1,
                 fields: Vec::new(),
             },
+            Layout {
+                name: String::from("name"),
+                kind: LayoutKind::String,
+                offset: 3,
+                fields: Vec::new(),
+            },
         ],
     };
 
-    let person_bytes = bincode::serialize(&person).unwrap();
-    let person_serialized = serialize(&person_layout, &person_bytes);
+    let person_typed = Person {
+        age: 27,
+        height: 180,
+        name: String::from("Bob is a cool kid"),
+    };
+    let person_two_typed = Person {
+        age: 50,
+        height: 150,
+        name: String::from("James D"),
+    };
+    println!("Person Typed: {:?}", person_typed);
+    println!("Person Two Typed: {:?}", person_two_typed);
 
-    println!("{}", person_serialized);
+    let person_bytes = bincode::serialize(&person_typed).unwrap();
+    let person_two_bytes = bincode::serialize(&person_two_typed).unwrap();
+    println!("Person Bytes: {:?}", person_bytes);
+    println!("Person Two Bytes: {:?}", person_two_bytes);
 
-    let person_bytes_after = deserialize(&person_layout, person_serialized);
+    let person_string = serialize(&person_layout, &person_bytes);
+    let person_two_string = serialize(&person_layout, &person_two_bytes);
+    println!("Person String: {:?}", person_string);
+    println!("Person Two String: {:?}", person_two_string);
 
-    println!("{:?}", get_in_slice::<u8>(&person_bytes, 0));
-    println!("{:?}", get_in_slice::<u8>(&person_bytes_after, 0));
+    let person_bytes = deserialize(&person_layout, &person_string);
+    let person_two_bytes = deserialize(&person_layout, &person_two_string);
+    println!("Person Bytes: {:?}", person_bytes);
+    println!("Person Two Bytes: {:?}", person_two_bytes);
 
-    let mut multiple_person_bytes = person_bytes_after
-        .iter()
-        .cloned()
-        .chain(person_bytes_after.iter().cloned())
-        .chain(person_bytes_after.iter().cloned())
-        .chain(person_bytes_after.iter().cloned())
-        .chain(person_bytes_after.iter().cloned())
-        .collect::<Vec<u8>>();
+    let person_typed = bincode::deserialize::<Person>(&person_bytes).unwrap();
+    let person_two_typed = bincode::deserialize::<Person>(&person_two_bytes).unwrap();
+    println!("Person Typed: {:?}", person_typed);
+    println!("Person Two Typed: {:?}", person_two_typed);
 
-    println!("Persons is {} bytes.", multiple_person_bytes.len());
+    //let mut multiple_person_bytes = person_bytes_after
+    //.iter()
+    //.cloned()
+    //.chain(person_bytes_after.iter().cloned())
+    //.chain(person_two_bytes.iter().cloned())
+    //.chain(person_bytes_after.iter().cloned())
+    //.chain(person_bytes_after.iter().cloned())
+    //.collect::<Vec<u8>>();
 
-    let mut multiple_person_typed = unsafe { from_bytes::<Person>(&mut multiple_person_bytes) };
+    //println!("Persons is {} bytes.", multiple_person_bytes.len());
 
-    multiple_person_typed[0].age = 10;
-    multiple_person_typed[1].age = 30;
-    multiple_person_typed[3].height = 120;
+    //let mut multiple_person_typed =
+    //bincode::deserialize::<[Person; 5]>(&multiple_person_bytes).unwrap();
 
-    println!("{:?}", multiple_person_typed[1]);
+    //multiple_person_typed[0].age = 10;
+    //multiple_person_typed[1].age = 30;
+    //multiple_person_typed[3].height = 120;
 
-    // Serialize and Deserialize array
+    //println!("{:?}", multiple_person_typed[1]);
+    //println!("{:?}", multiple_person_typed[2]);
 
-    let multiple_person_serialized = serialize(&person_layout, &multiple_person_bytes);
+    //// Serialize and Deserialize array
 
-    println!("Multiple Serialized: {}", multiple_person_serialized);
+    //let multiple_person_serialized = serialize(&person_layout, &multiple_person_bytes);
 
-    let mut multiple_person_deserialized = deserialize(&person_layout, multiple_person_serialized);
+    //println!("Multiple Serialized: {}", multiple_person_serialized);
 
-    let multiple_person_rebuilt =
-        unsafe { from_bytes::<Person>(&mut multiple_person_deserialized) };
+    //let mut multiple_person_deserialized = deserialize(&person_layout, multiple_person_serialized);
 
-    println!("{:?}", multiple_person_rebuilt);
+    //let multiple_person_rebuilt =
+    //unsafe { from_bytes::<Person>(&mut multiple_person_deserialized) };
+
+    ////println!("{:?}", multiple_person_rebuilt);
 }
